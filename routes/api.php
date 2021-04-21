c1be7539fc8b5cff1242da18130b5154b04b2662
unknown <elsayedkamal581999@gmail.com>
2021-04-21 21:15:48 +0200

    “wip”

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Api\User\MeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::middleware(['guest'])->group(function () {
     Route::post('register', [AuthController::class, "register"])->name('register');
     Route::post('login', [AuthController::class, "login"])->name('login');
